5991b818103a59bef9fc36cfbc493d524f6857bb
MaisLaufen <vip.eftifanov@mail.ru>
2025-07-22 03:57:37 +0700

    [Add] Theme and palette files

---
+++ src/presentation/palette.ts
@@ -0,0 +1,12 @@
+export const Palette = {
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#232323',
+  darkGray: '#111111',
+  lightGray: '#393939',
+  red: '#FF4C4C',
+  cuteRed: '#ff5050',
+  blue: '#007AFF',
+  green: '#15e030',
+  yellow: '#FFCC00',
+};
---
+++ src/presentation/theme.ts
@@ -0,0 +1,26 @@
+import { Palette } from './palette';
+
+export const Theme = {
+    
+    // Main colors:
+    primaryColor: Palette.black,
+    secondaryColor: Palette.white,
+    tertiaryColor: Palette.yellow,
+
+    // Text colors:
+    textPrimary: Palette.black,
+    textSecondary: Palette.white,
+    textHref: Palette.blue,
+
+    // Icon colors:
+    iconPrimary: Palette.black,
+    iconSecondary: Palette.white,
+
+    // Modal colors:
+    primaryModalColor: Palette.darkGray,
+    secondaryModalColor: Palette.gray,
+
+    // Info colors:
+    error: Palette.red,
+    like: Palette.cuteRed,
+};
