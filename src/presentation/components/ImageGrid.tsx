0e39004f3e77bac6b69a3e5a2e82a442a84c8014
MaisLaufen <vip.eftifanov@mail.ru>
2025-07-22 02:19:40 +0700

    Beatify animation while imageGrid loading

--- src/presentation/components/ImageGrid.tsx
+++ src/presentation/components/ImageGrid.tsx
@@ -7,37 +7,54 @@ import {
   PanResponder,
   Text,
   Animated,
+  Dimensions,
 } from 'react-native';
 import { ImageModel as ImageModel } from '../../domain/models/image';
 import { ImageCard } from './ImageCard';
 
+const windowHeight = Dimensions.get('window').height;
+
 interface Props {
   images: ImageModel[];
   loadingMore: boolean;
-  onEndReached: () => void;
   navigation: any;
 }
 
-export const ImageGrid = ({ images, loadingMore, onEndReached, navigation }: Props) => {
-
+export const ImageGrid = ({ images, loadingMore, navigation }: Props) => {
   return (
     <>
       <FlatList
         data={images}
         keyExtractor={(item) => item.id}
         numColumns={3}
-        renderItem={({ item, index }) => (
-          <ImageCard image={item} 
-          onPress={() => navigation.navigate('ImageDetail', {image: item})}/>
+        renderItem={({ item }) => (
+          <ImageCard
+            image={item}
+            onPress={() => navigation.navigate('ImageDetail', { image: item })}
+          />
         )}
         contentContainerStyle={styles.list}
         columnWrapperStyle={styles.row}
-        onEndReached={onEndReached}
         onEndReachedThreshold={0.5}
-        ListFooterComponent={
-          loadingMore ? <ActivityIndicator size="small" style={{alignContent: 'center'}} /> : null
-        }
       />
+
+      {loadingMore && (
+        <View style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          height: windowHeight,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black', // полупрозрачный фон для затемнения
+          zIndex: 1000,
+        }}>
+          <ActivityIndicator size="large" color='yellow' />
+          <Text style={{ marginTop: 8, fontSize: 20 }}>Загрузка...</Text>
+        </View>
+      )}
     </>
   );
 };
